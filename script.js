90b46c83f19e4a6723483920c469819aee93e65c
Sascha-La <al.artishevskaya98@gmail.com>
2024-09-29 19:48:41 +0300

    add JS code
    add JS code

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
+const itemsContainer = document.querySelector("#items");
+const itemsInput = document.querySelector("#input");
+
+itemsContainer.addEventListener('keydown', function() {
+    const itemText = itemsInput.value;
+    var keyCode = event.keyCode;
+
+    const newItem = document.createElement('div');
+    newItem.classList.toggle("done");
+    newItem.textContent = itemText;
+
+    if (itemText != '' && keyCode == Enter) {
+        itemsContainer.append(newItem);
+
+        itemsInput.value = '';
+    }
+});
+
 /* Пустые элементы не должны добавляться */
 
 /* Если кликнуть на элемент списка, он зачеркивается */
